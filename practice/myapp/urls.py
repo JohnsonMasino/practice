5e725fc4d0477d9cf433a4617ede6d68df40a199
Johnson Masino <johnsonmasino@gmail.com>
2023-11-15 12:07:21 +0100

    Pushing Latest Changes

--- practice/myapp/urls.py
+++ practice/myapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("first/", views.first, name="first"),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='result'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
