c0c640677852ad618fe4d00e00fe6c32625670f9
Johnson Masino <johnsonmasino@gmail.com>
2023-11-15 10:41:37 +0100

    Commiting the latest changes

--- practice/myapp/views.py
+++ practice/myapp/views.py
@@ -2,6 +2,4 @@ from django.http import HttpResponse
 
 def first(request):
     return HttpResponse('This statement here '
-                        'is coming from the app '
-                        'Nonetheless written by Masino')
-
+                        'is coming from the app')
