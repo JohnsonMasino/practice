fab5e570e0196f20d6c764eaed73d6c1f8f96eee
Johnson Masino <johnsonmasino@gmail.com>
2023-11-20 01:30:11 +0100

    Updating views and urls

--- practice/myapp/urls.py
+++ practice/myapp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("first/", views.first, name="first"),
     path('', views.index, name='index'),
     path('detail/<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='result'),
+    path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
